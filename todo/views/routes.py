4fd13006a42bba57fdf45b41777b20ac5557bf0d
Theodore Kim <tkim0476@gmail.com>
2025-02-28 09:34:47 +1000

    buh

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -6,22 +6,10 @@ api = Blueprint('api', __name__, url_prefix='/api/v1')
 def health():
     return jsonify({"status":"ok"})
 
-# @api.route('/todos', methods=['GET'])
-# def get_todos():
-#     return jsonify([{
-#         "id": 1,
-#         "title": "Watch CSSE6400 Lecture",
-#         "description": "Watch the CSSE6400 lecture on ECHO360 for week 1", 
-#         "completed": True, 
-#         "deadline_at": "2023-02-27T00:00:00", 
-#         "created_at": "2023-02-20T00:00:00", 
-#         "updated_at": "2023-02-20T00:00:00" 
-#     }])
-
-@api.route('/todos/<int:id>', methods=['GET'])
-def get_todos(id):
+@api.route('/todos', methods=['GET'])
+def get_todos():
     return jsonify([{
-        "id": id,
+        "id": 1,
         "title": "Watch CSSE6400 Lecture",
         "description": "Watch the CSSE6400 lecture on ECHO360 for week 1", 
         "completed": True, 
@@ -29,6 +17,18 @@ def get_todos(id):
         "created_at": "2023-02-20T00:00:00", 
         "updated_at": "2023-02-20T00:00:00" 
     }])
+
+@api.route('/todos/<int:id>', methods=['GET'])
+def get_todo(id):
+    return jsonify({
+        "id": id,
+        "title": "Watch CSSE6400 Lecture",
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1", 
+        "completed": True, 
+        "deadline_at": "2023-02-27T00:00:00", 
+        "created_at": "2023-02-20T00:00:00", 
+        "updated_at": "2023-02-20T00:00:00" 
+    })
 
 @api.route('/todos', methods=['POST'])
 def create_todo():
